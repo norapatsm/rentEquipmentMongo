05b8a111004c0c28d950aa85858f6312ecc8e849
nor <norapat.sm@gmail.com>
2023-02-23 15:27:34 +0700

    debug

--- models/admin_model.js
+++ models/admin_model.js
@@ -16,7 +16,7 @@ db.on('error', (err) => {
 
 const adminSchema = new mongoose.Schema({
     username: { type: String, trim: true, required: true },
-    password: { type: String, trim: true, required: true },}
-)
+    password: { type: String, trim: true, required: true },
+})
 
 module.exports = mongoose.model('admin', adminSchema);
